8c1991e054ea070d3a14d5985fe0da18677e9e08
Mikel Arnaiz <mikel.arnaiz@gmail.com>
2019-02-28 19:32:40 +0100

    Updates

---
+++ precache-manifest.612aa4fb2c46f2b08690c1875b6f3160.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mikel.dev/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7282f4c49fe385b307fc",
+    "url": "/mikel.dev/static/js/runtime~main.7282f4c4.js"
+  },
+  {
+    "revision": "72792922252a50695ad0",
+    "url": "/mikel.dev/static/js/main.72792922.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/mikel.dev/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "72792922252a50695ad0",
+    "url": "/mikel.dev/static/css/main.7a3a0326.chunk.css"
+  },
+  {
+    "revision": "eb5ee2a1dc65d2b90621deb0b590f48e",
+    "url": "/mikel.dev/index.html"
+  }
+];
